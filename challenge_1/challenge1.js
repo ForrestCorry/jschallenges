416b9f31718ddeddcc8f3b9b7949981286d8f209
Forrest <forrest.corry@gmail.com>
2016-09-13 11:42:44 -0400

    challengeOne commit

--- challenge_1/challenge1.js
+++ challenge_1/challenge1.js
@@ -10,18 +10,33 @@
 var saveButton = $('#save-button');
 var loadButton = $('#load-button');
 
+ 
+
 /**
  * Gets the text from the element for you
  * @return {String}
  */
 function getText() {
+ 
   return $('#save-me').val();
-}
+  
+};
 
 /**
  * Puts different text
  * @param {String} text the stuff you want to put in the box
  */
 function setText(text) {
+   
   return $('#save-me').val(text);
-}
+
+  };
+  $("#save-button").click(function(){
+  	localStorage.setItem('inputText', getText());
+  	setText('');
+  })
+  $("#load-button").click(function(){
+  	setText(localStorage.getItem('inputText'));
+  })
+
+
