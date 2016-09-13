48843d8b46f063e399ca15f051c88df45e5d8a17
Forrest <forrest.corry@gmail.com>
2016-09-13 15:13:18 -0400

    challenge2 commit

--- challenge_2/challenge2.js
+++ challenge_2/challenge2.js
@@ -1,5 +1,25 @@
 $(function(){
+    var replaced = $("body").html().replace(/Bacon/g,'Laser Vision');
+		$("body").html(replaced);
 
-	//all your code should go here
 
-})
+    var state = true;
+    $("h1").click(function(){
+    	if (state){
+    		$("h1").css({color: "red"});
+    	}else{
+    		$("h1").css({color: "black"});
+    	}
+    	state = !state;
+     });
+
+    $('.title:even').hide();
+
+	$('.post:last').remove();
+	$('.post:last').remove();
+	$('.post:last').remove();
+
+	$('aside').remove();
+	$('footer li:even').hide();
+
+});
